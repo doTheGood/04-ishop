0f7fdccafd4bfa43db0e16080cdc23ab78f18b33
Diogo Novaes <diogonovaes@gmail.com>
2022-09-14 10:46:01 +0200

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,20 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { globalStyles } from '../styles/global'
+import logoImg from '../assets/logo.svg'
+import { AppProps } from 'next/app'
+import Image from 'next/future/image'
+import { Container, Header } from '../styles/pages/app'
+
+globalStyles()
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
 
 export default MyApp
