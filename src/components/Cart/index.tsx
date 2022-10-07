905e27a4295e50581e5c9e185f5f22aae4984565
Diogo Novaes <diogo.dasilva@emea.adecco.net>
2022-10-07 21:30:40 +0200

    interface updated to the shop new upgrade

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -7,7 +7,9 @@ import {
   CartContent,
   CartFinalization,
   CartProduct,
+  CartProductDetails,
   CartProductImage,
+  FinalizationDetails,
 } from './styles';
 
 export function Cart() {
@@ -35,10 +37,27 @@ export function Cart() {
                   src="https://s3-alpha-sig.figma.com/img/387d/13ce/de131bd1ccf9bbe6b2331e88d3df20cd?Expires=1665964800&Signature=eRip5zM0xcid60KsBVoA1e4VB9XM-2rtr1wfN5UGqzM0y0if6NDCIF4f8n62xOO~JCuQnCZ5k~xx-C1XgCbXs4luHdzfhBOQhUmqfxMNfwf61i8rsboFeGPvKF5qJWYNLSiRq05KwWvIDB6LZysA4FN86fAsbN0ytJ~s0yxlWjZmxFxr46B9842Na1j-OZR2wYlR6F~6ctfaPXtSZajM-Eubd6D34fQFKKuZ2m6Jp4JVBBPHhQODonL6xWTIP0TW6lNMRaALapIhGeafIjHxxJ-GnnF4FF5JFjeCKo8y4v8dS46f9Ft4JxOYKq8TNxaxrJJHthYSo1SzCGGg3pcR0A__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
                 />
               </CartProductImage>
+              <CartProductDetails>
+                <p>Produto 1</p>
+                <strong>R$ 50.00</strong>
+                <button>Remover</button>
+              </CartProductDetails>
             </CartProduct>
           </section>
 
-          <CartFinalization></CartFinalization>
+          <CartFinalization>
+            <FinalizationDetails>
+              <div>
+                <span>Quantidade</span>
+                <p>2 itens</p>
+              </div>
+              <div>
+                <span>Valor total</span>
+                <p>R$ 100.00</p>
+              </div>
+            </FinalizationDetails>
+            <button>Finalizar compra</button>
+          </CartFinalization>
         </CartContent>
       </Dialog.Portal>
     </Dialog.Root>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,66 +1,79 @@
-import Image from 'next/future/image'
-import { HomeContainer, Product } from '../styles/pages/home'
+import Image from 'next/future/image';
+import { HomeContainer, Product, SliderContainer } from '../styles/pages/home';
 
-import { useKeenSlider } from 'keen-slider/react'
-import 'keen-slider/keen-slider.min.css'
-import { GetStaticProps } from 'next'
-import { stripe } from '../lib/stripe'
-import Stripe from 'stripe'
-import Link from 'next/link'
-import Head from 'next/head'
+import { GetStaticProps } from 'next';
+import { stripe } from '../lib/stripe';
+import Stripe from 'stripe';
+import Link from 'next/link';
+import Head from 'next/head';
+import useEmblaCarousel from 'embla-carousel-react';
+import { CartButton } from '../components/CartButton';
 
 interface IHomeProps {
   products: {
-    id: string
-    name: string
-    imageUrl: string
-    price: number
-  }[]
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: number;
+  }[];
 }
 
 export default function Home({ products }: IHomeProps) {
-  const [sliderRef] = useKeenSlider({
-    slides: {
-      perView: 3,
-      spacing: 48,
-    },
-  })
+  const [emblaRef] = useEmblaCarousel({
+    align: 'start',
+    skipSnaps: false,
+    dragFree: true,
+  });
 
   return (
     <>
       <Head>
         <title>Home | I-shop-04</title>
       </Head>
-      <HomeContainer ref={sliderRef} className="keen-slider">
-        {products.map((product) => {
-          return (
-            <Link
-              key={product.id}
-              href={`product/${product.id}`}
-              prefetch={false}
-            >
-              <Product className="keen-slider__slide">
-                <Image src={product.imageUrl} width={520} height={480} alt="" />
-                <footer>
-                  <strong>{product.name}</strong>
-                  <span>{product.price}</span>
-                </footer>
-              </Product>
-            </Link>
-          )
-        })}
-      </HomeContainer>
+      <div style={{ overflow: 'hidden', width: '100%' }}>
+        <HomeContainer>
+          <div className="embla" ref={emblaRef}>
+            <SliderContainer className="embla__container container">
+              {products.map((product) => {
+                return (
+                  <Link
+                    key={product.id}
+                    href={`product/${product.id}`}
+                    prefetch={false}
+                  >
+                    <Product className="embla__slide">
+                      <Image
+                        src={product.imageUrl}
+                        width={520}
+                        height={480}
+                        alt=""
+                      />
+                      <footer>
+                        <div>
+                          <strong>{product.name}</strong>
+                          <span>{product.price}</span>
+                        </div>
+                        <CartButton color="green" size="large" />
+                      </footer>
+                    </Product>
+                  </Link>
+                );
+              })}
+            </SliderContainer>
+          </div>
+        </HomeContainer>
+      </div>
     </>
-  )
+  );
 }
 
 export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price'],
-  })
+  });
 
   const products = response.data.map((product) => {
-    const price = product.default_price as Stripe.Price
+    const price = product.default_price as Stripe.Price;
 
     return {
       id: product.id,
@@ -70,13 +83,13 @@ export const getStaticProps: GetStaticProps = async () => {
         style: 'currency',
         currency: 'EUR',
       }).format(price.unit_amount / 100),
-    }
-  })
+    };
+  });
 
   return {
     props: {
       products,
     },
     revalidate: 60 * 60 * 2,
-  }
-}
+  };
+};
